ca1bf5f3fbffcf264ff1a52647987ab91dbf9449
Ross Cruickshank <ross.cruickshank@uk.ibm.com>
2026-07-29 15:41:21 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -121,6 +121,10 @@ router.get('/info', async (req, res) => {
 })
 
 // Wildcard route to deal with redirecting to React routes
-router.get('*', (req, res) => { console.log("* req", req);res.sendFile(path.join(__dirname, '../public/index.html')) } )
+router.get('*', (req, res) => { 
+  res.sendFile(path.join(__dirname, '../public/lti.html'))
+  console.log("* req", req);
+}
+ //                              res.sendFile(path.join(__dirname, '../public/index.html')) } )
 
 module.exports = router
